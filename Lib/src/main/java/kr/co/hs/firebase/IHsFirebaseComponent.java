98c12277d0219584a70783786d4408ee1fdadf94
hsbaewa <hsbaewa@gmail.com>
2017-01-17 19:14:13 +0900

    수정

---
+++ Lib/src/main/java/kr/co/hs/firebase/IHsFirebaseComponent.java
@@ -0,0 +1,12 @@
+package kr.co.hs.firebase;
+
+import kr.co.hs.app.IHsComponent;
+
+/**
+ * 생성된 시간 2017-01-12, Bae 에 의해 생성됨
+ * 프로젝트 이름 : MobileFilter4
+ * 패키지명 : kr.co.hs.firebase
+ */
+
+public interface IHsFirebaseComponent extends IHsComponent, IHsFirebase {
+}
