23bcc89ee69153b3b77b5513f423f2196766e14e
hsbaewa <hsbaewa@gmail.com>
2017-02-14 11:56:48 +0900

    HsCommon 버전 업 및 SearchView를 활용한 Activity, Fragment 추가

---
+++ Lib/src/main/java/kr/co/hs/firebase/app/HsFirebaseSearchFragment.java
@@ -0,0 +1,244 @@
+package kr.co.hs.firebase.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.RemoteMessage;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.Map;
+
+import kr.co.hs.app.HsSearchFragment;
+import kr.co.hs.firebase.IHsFirebaseApplication;
+import kr.co.hs.firebase.IHsFirebaseFragment;
+import kr.co.hs.firebase.auth.HsFirebaseAuth;
+import kr.co.hs.firebase.cloudmessaging.HsFirebaseMessagingInfo;
+
+/**
+ * 생성된 시간 2017-02-14, Bae 에 의해 생성됨
+ * 프로젝트 이름 : HsFirebase
+ * 패키지명 : kr.co.hs.firebase.app
+ */
+
+public abstract class HsFirebaseSearchFragment extends HsSearchFragment implements IHsFirebaseFragment{
+
+    @Override
+    public HsFirebaseApplication getHsFirebaseApplication() {
+        HsFirebaseApplication application = (HsFirebaseApplication) getHsApplication();
+        return application;
+    }
+
+    @Override
+    public String getFirebaseToken() {
+        IHsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            return application.getFirebaseToken();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean sendFirebaseAnalyticsLogEvent(String name, Bundle data) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null)
+            return application.sendFirebaseAnalyticsLogEvent(name, data);
+        return false;
+    }
+
+    @Override
+    public boolean setFirebaseAnalyticsUserId(String id) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null)
+            return application.setFirebaseAnalyticsUserId(id);
+        return false;
+    }
+
+    @Override
+    public boolean setFirebaseAnalyticsCurrentScreen(Activity activity, String screenName, String screenClassOverride) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null)
+            return application.setFirebaseAnalyticsCurrentScreen(activity, screenName, screenClassOverride);
+        return false;
+    }
+
+    @Override
+    public void createUserWithEmailAndPassword(String email, String password, HsFirebaseAuth.OnFirebaseAuthResultListener listener) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null)
+            application.createUserWithEmailAndPassword(email, password, listener);
+    }
+
+    @Override
+    public void signInWithEmailAndPassword(String email, String password, HsFirebaseAuth.OnFirebaseAuthResultListener listener) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null)
+            application.signInWithEmailAndPassword(email, password, listener);
+    }
+
+    @Override
+    public void signOut() {
+        signOut(null);
+    }
+
+    @Override
+    public void signOut(HsFirebaseAuth.OnFirebaseSignOutResultListener listener) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null)
+            application.signOut(listener);
+    }
+
+    @Override
+    public HsFirebaseAuth getFirebaseAuth() {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null)
+            return application.getFirebaseAuth();
+        return null;
+    }
+
+    @Override
+    public FirebaseUser getFirebaseUser() {
+        HsFirebaseAuth auth = getFirebaseAuth();
+        if(auth != null)
+            return auth.getCurrentUser();
+        else
+            return null;
+    }
+
+    @Override
+    public void signInWithCredential(AuthCredential credential, HsFirebaseAuth.OnFirebaseAuthResultListener listener) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null)
+            application.signInWithCredential(credential, listener);
+    }
+
+    @Override
+    public boolean subscribe(String topic) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            return application.subscribe(topic);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean unsubscribe(String topic) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            return application.unsubscribe(topic);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean send(RemoteMessage remoteMessage) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            return application.send(remoteMessage);
+        }
+        return false;
+    }
+
+    @Override
+    public HsFirebaseMessagingInfo getFirebaseMessagingInfo(String serverKey) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            return application.getFirebaseMessagingInfo(serverKey);
+        }
+        return null;
+    }
+
+    @Override
+    public boolean send(String serverKey, String to, Map<String, String> payload, HsFirebaseMessagingInfo.OnSendResultListener listener) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            return application.send(serverKey, to, payload, listener);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean send(String serverKey, String[] to, Map<String, String> payload, HsFirebaseMessagingInfo.OnSendResultListener listener) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            return application.send(serverKey, to, payload, listener);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean sendToTopic(String serverKey, String to, Map<String, String> payload, HsFirebaseMessagingInfo.OnSendResultListener listener) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            return application.sendToTopic(serverKey, to, payload, listener);
+        }
+        return false;
+    }
+
+    @Override
+    public FirebaseDatabase getFirebaseDatabase() {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            return application.getFirebaseDatabase();
+        }
+        return null;
+    }
+
+    @Override
+    public DatabaseReference getDatabaseReference() {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            return application.getDatabaseReference();
+        }
+        return null;
+    }
+
+    @Override
+    public DatabaseReference child(String key) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            return application.child(key);
+        }
+        return null;
+    }
+
+    @Override
+    public Task<Void> updateChildren(Map<String, Object> map) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            return application.updateChildren(map);
+        }
+        return null;
+    }
+
+    @Override
+    public void updateChildren(Map<String, Object> map, DatabaseReference.CompletionListener listener) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            application.updateChildren(map, listener);
+        }
+    }
+
+    @Override
+    public FirebaseStorage getFirebaseStorage() {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            return application.getFirebaseStorage();
+        }
+        return null;
+    }
+
+    @Override
+    public StorageReference getStorageReference(String Url) {
+        HsFirebaseApplication application = getHsFirebaseApplication();
+        if(application != null){
+            return application.getStorageReference(Url);
+        }
+        return null;
+    }
+}
